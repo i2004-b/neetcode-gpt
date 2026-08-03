26e949d79080a0900db03efb4b1095a5d7a4e0e0
Ivan <ivanbyju.2023@gmail.com>
2026-08-03 15:20:47 -0400

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -9,11 +9,13 @@ class Solution:
         # y_pred: predicted probabilities
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
-
-        # Should clip value because adding epsilon to 1 would result in negative 
+        
+        # Clip the values so you can avoid log(0)
         y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
-        b_c_e = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
-        return np.round(b_c_e, 4)
+        # Binary cross entropy finds the mean of all such values
+        binary_ce = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+        return np.round(binary_ce, 4)
 
     def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
         # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
@@ -21,8 +23,9 @@ class Solution:
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
         
-        # Should clip value because adding epsilon to 1 would result in negative
+        # Clip the values to avoid log(0)
         y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
-        # Take the sum of the multiplications and divide by the length
-        c_c_e = -np.sum(y_true * np.log(y_pred)) / len(y_true)
-        return np.round(c_c_e, 4)
+        # For categorical cross entropy, divide by the length of the examples
+        # Inside, you are just summing the multiplication of the actual times the natural log of the probability
+        categorical_ce = -np.sum(y_true * np.log(y_pred)) / len(y_true)
+        return np.round(categorical_ce, 4)
